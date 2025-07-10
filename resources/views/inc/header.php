64596470d8c58a890b7a07aafce5e03678e3ff6f
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-07-10 09:42:07 +0500

    logo and fav icon added

--- resources/views/inc/header.php
+++ resources/views/inc/header.php
@@ -2,13 +2,15 @@
 
 use App\Core\Helper;
 
- $helper = new Helper(); ?>
+$helper = new Helper(); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marhaba Technical Services - We take care your Home Devices runs smoothly</title>
+    <title>Marhaba Technical Services | Expert Appliance Repair in Sharjah</title>
+    <meta name="description" content="Marhaba Technical Services offers expert repair and installation for ACs, washing machines, refrigerators, and more in Dubai. Fast, reliable, and affordable." />
     <link rel="stylesheet" href="<?php echo $helper::asset('css/variables.css')  ?>">
     <link rel="stylesheet" href="<?php echo $helper::asset('css/main.css')  ?>">
     <link rel="stylesheet" href="<?php echo $helper::asset('css/header.css')  ?>">
@@ -18,10 +20,12 @@ use App\Core\Helper;
     <link rel="stylesheet" href="<?php echo $helper::asset('css/maintain.css')  ?>">
     <link rel="stylesheet" href="<?php echo $helper::asset('css/notfound.css')  ?>">
     <link rel="stylesheet" href="<?php echo $helper::asset('fa-6.7.2/css/all.min.css')  ?>">
-    </head>
+    <link rel="shortcut icon" href="/images/icon.png" type="image/x-icon">
+</head>
+
 <body>
 
-<?php
-// css path
-// $var = str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(__DIR__.'/../../css/app.css')); 
-include 'navbar.php';
+    <?php
+    // css path
+    // $var = str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(__DIR__.'/../../css/app.css')); 
+    include 'navbar.php';
--- resources/views/inc/navbar.php
+++ resources/views/inc/navbar.php
@@ -1,7 +1,9 @@
 <header class="header">
     <div class="topbar">
         <div class="left">
-            <div class="address"><address> <i class="fa-solid fa-location-dot"></i>Sharjah</address></div>
+            <div class="address">
+                <address> <i class="fa-solid fa-location-dot"></i>Sharjah</address>
+            </div>
             <div class="email"><a href="mailto:info@marhabatechnicalservices.com"><i class="fa-solid fa-envelope"></i>info@marhabatechnicalservices.com</a></div>
             <div class="phone"><a href="tel:+0563447645"> <i class="fa-solid fa-phone"></i> 0563447645 </a></div>
         </div>
@@ -13,20 +15,28 @@
         </div>
     </div>
     <nav class="navbar">
-        <div class="logo">
-            <!-- <img src="" alt=""> -->
-            <h2 class="logo"><a href="/"><i class="fa-solid fa-gear"></i> Marhaba Technical Services</a></h2>
+        <div class="logo-container">
+            <?php
+            $logo = true;
+            if ($logo):
+            ?>
+                <img class="logo" src="/images/trnsp-logo.png" alt="Marhaba Technical Services Logo">
+            <?php else: ?>
+                <h2 class="logo"><a href="/"><i class="fa-solid fa-gear"></i> Marhaba Technical Services</a></h2>
+            <?php endif ?>
         </div>
-             <ul class="nav-links">
-                <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
-                <li class="nav-item"><a href="/services" class="nav-link">Services</a></li>
-                <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
-                <li class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
-                
-             </ul>
+        <ul class="nav-links">
+            <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="/services" class="nav-link">Services</a></li>
+            <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
+            <li class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
+
+        </ul>
         <div class="cta">
-            <button class="support"><a href="tel:+0563447645 "><i class="fa-solid fa-headset"></i><p>Quick Call</p></a></button>
+            <button class="support"><a href="tel:+0563447645 "><i class="fa-solid fa-headset"></i>
+                    <p>Quick Call</p>
+                </a></button>
         </div>
     </nav>
 </header>
